c4c331d84af0aa77aba758342d57a70d7d9d09c3
Bebe Jane <bjornthief@gmail.com>
2023-11-18 18:46:48 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,7 @@
 import { cache } from 'react'
 import styles from './page.module.css'
 
-const testCache = cache((opt?: any) => {
-  console.log('cache', opt?.t)
-  return Date.now()
-})
-
-export default async function Home() {
-
+const getData = cache(async (): Promise<any> => {
   const res = await fetch('https://random-data-api.com/api/v2/users?response_type=json', {
     method: 'GET',
     headers: {
@@ -20,14 +14,19 @@ export default async function Home() {
       tags: ['page', 'test', 'hello']
     }
   })
+  return res.json()
+})
 
-  const data = await res.json()
+export default async function Home() {
+
+  const data = await getData()
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         {data.username}
       </div>
+      <a href="/revalidate?tags=page" target="_blank">Revalidate</a>
     </main>
   )
 }
